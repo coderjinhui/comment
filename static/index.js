e3860d3b6e38e92b2091f3c4cccd1ba691b82450
maicss <maicss@foxmail.com>
2018-08-02 17:40:39 +0800

    8.2

--- static/index.js
+++ static/index.js
@@ -7,6 +7,9 @@ $(function () {
   const userSelect = $('#user-select')
   const userInfoDiv = $('#user-info')
   const upVoteArticle = $('#article-up-vote')
+  const userHeader = $('#user-header')
+  const commentContent = $('#comment-text')
+  const submitComment = $('#submit-comment')
 
   let selectedUserId = ''
   let userInfo = {}
@@ -20,16 +23,17 @@ $(function () {
       articleContent.html(d.content.split('。').join('。<br>'))
       upVoteArticle.html('点赞(' + (article.upVoteCount) + ')')
     })
-    .catch(e => errorMessageP.html(e).show())
+    .catch(e => errorMessageP.html(e.statusText).show())
 
   $.get('/user')
     .then(d => {
       users = d
       selectedUserId = d[0].id
       userInfo = d[0]
+      userHeader.html(userInfo.name[0])
       if (userInfo.upVoteArticles.some(record => record.article === article.id)) {
         upVoteArticle.html('已点赞(' + article.upVoteCount + ')')
-    }
+      }
       userInfoDiv.html(JSON.stringify(userInfo))
       $.each(d, (i, user) => {
         userSelect.append($('<option>', {
@@ -38,27 +42,39 @@ $(function () {
         }))
       })
     })
-    .catch(e => errorMessageP.html(e).show())
+    .catch(e => errorMessageP.html(e.statusText).show())
 
   userSelect.on('change', function () {
     selectedUserId = this.value
-    userInfo = users.filter(user => user.id === selectedUserId)
+    userInfo = users.filter(user => user.id === selectedUserId)[0]
+    userHeader.html(userInfo.name[0])
     userInfoDiv.html(JSON.stringify(userInfo))
   })
   upVoteArticle.on('click', function () {
-    // todo 这个判断标准不好
-    const btnText = upVoteArticle.html()
-    if (btnText.includes('已点赞')) return
+    if (userInfo.upVoteArticles.find(record => record.article === article.id)) {
+      return
+    }
     upVoteArticle.html('已点赞(' + (article.upVoteCount + 1) + ')')
     $.post('/upVoteArticle', {user: selectedUserId, article: article.id, timestamp: Date.now()})
       .then((d => {
         console.log(d)
       }))
   })
+  submitComment.on('click', function () {
+    const _commentContent = commentContent.html().trim()
+    if (!_commentContent) return
+    $.ajax({
+      url: '/comments/add',
+      method: 'PUT',
+      data: {content: _commentContent, userId: userInfo.id, articleId: article.id}
+    })
+      .then(d => {})
+      .catch(e => errorMessageP.html(e.statusText).show())
+  })
 
   $.get('/comments/article/0001').then(d => {
 
-  }).catch(e => errorMessageP.html(e).show())
+  }).catch(e => errorMessageP.html(e.statusText).show())
 
   articleComment.on('click', function () {
 

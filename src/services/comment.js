187621380e47dea0cbb9ff444d71cfa9fa83d569
maicss <maicss@foxmail.com>
2018-08-03 10:09:30 +0800

    tests for services/comment

--- src/services/comment.js
+++ src/services/comment.js
@@ -2,6 +2,13 @@ const mongo = require('../mongo')
 const {collections, projection} = require('../config')
 const {genUUID} = require('../utils')
 
+/**
+ * @description get comments by some filter
+ * @param [articleId] {string}
+ * @param [userId] {string}
+ * @param [commentId] {string}
+ * @returns {Promise<comment>}
+ */
 const getCommentsByFilter = async ({articleId, userId, commentId}) =>{
   const query = {}
   if (articleId) query.articleId = articleId
@@ -13,6 +20,7 @@ const getCommentsByFilter = async ({articleId, userId, commentId}) =>{
 const getOneComment = async id => await mongo.db.collection(collections.COMMENT).findOne({id}, projection)
 
 /**
+ * @function
  * @description 增加一个评论
  * @param userId {string}
  * @param articleId {string}
@@ -21,21 +29,32 @@ const getOneComment = async id => await mongo.db.collection(collections.COMMENT)
  * @return {promise} the comment added
  * */
 const addOneComment = async (userId, articleId, content, commentId=null) =>
-  await mongo.db.collection(collections.COMMENT).insertOne({
+  (await mongo.db.collection(collections.COMMENT).insertOne({
     userId, commentId, articleId, content,
     id: genUUID(),
     createTime: Date.now(),
     isDeleted: false,
-  }, projection)
+  })).ops[0]
 
-const deleteOneComment = async ({id, articleId, userId}) =>
-  await mongo.db.collection(collections.COMMENT).findOneAndUpdate({id}, {
+/**
+ * @function
+ * @param id comment id
+ * @param [articleId]
+ * @param [userId]
+ * @return {promise}
+ */
+const deleteOneComment = async (id, {articleId, userId}) => {
+  const updateRole = articleId ? 'article' : 'user'
+  const result = await mongo.db.collection(collections.COMMENT).findOneAndUpdate({id}, {
     $set: {
       isDeleted: true,
       deletedTime: Date.now(),
-      deletedBy: articleId || userId
+      deletedBy: articleId || userId,
+      updater: updateRole
     }
-  }, projection)
+  }, {...projection, returnOriginal: false})
+  return result.value
+}
 
 /**
  * @description 批量删除文章的评论
@@ -46,7 +65,9 @@ const deleteCommentsByArticle = async articleId =>
   await mongo.db.collection(collections.COMMENT).updateMany({articleId}, {
     $set: {
       isDeleted: true,
-      deletedTime: Date.now()
+      deletedTime: Date.now(),
+      deletedBy: articleId,
+      updater: 'article'
     }
   })
 
--- test/services/comments.test.js
+++ test/services/comments.test.js
@@ -3,21 +3,42 @@ const mongo = require('../../src/mongo')
 const services = require('../../src/services')
 const {articles} = require('../../src/config')
 
-describe('service/comments.js', function(){
+describe('service/comments.js', function () {
 
-    const articleId = '0001'
-    const userId = '0001'
+  const articleId = '0001'
+  const userId = '0001'
 
-    beforeEach(async function () {
-        return await mongo.init()
-    })
+  const commentContent = '1122some aad'
+  let commentId
 
-    it('get comments by article id', async () => {
-        assert((await services.comment.getCommentsByFilter({articleId})).length === 1)
-    })
+  beforeEach(async function () {
+    return await mongo.init()
+  })
 
-    it('get comments by user id', async () => {
-        assert((await services.comment.getCommentsByFilter({userId})).length === 1)
-    })
+  it('get comments by article id', async () => {
+    assert((await services.comment.getCommentsByFilter({articleId})).length === 1)
+  })
+
+  it('get comments by user id', async () => {
+    assert((await services.comment.getCommentsByFilter({userId})).length === 1)
+  })
+
+  it('add one comment', async () => {
+    const comment = await services.comment.addOneComment(userId, articleId, commentContent)
+    commentId = comment.id
+    assert(comment.content === commentContent)
+  })
+
+  it('remove one comment', async () => {
+    const res = await services.comment.deleteOneComment(commentId, {userId})
+    assert(res.id === commentId)
+    assert(res.isDeleted === true)
+    assert(res.deletedBy)
+  })
+
+  it('remove all comments under article', async () => {
+    const res = await services.comment.deleteCommentsByArticle(articleId)
+    assert(res.result.ok === 1)
+  })
 
 })

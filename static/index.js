8515bd37d5c1e0c59b4dfa8c0b0f8a0ea7a6fbba
maicss <maicss@foxmail.com>
2018-08-09 17:19:42 +0800

    准备抄个评论界面

--- static/index.js
+++ static/index.js
@@ -1,5 +1,21 @@
-const $ = window.jQuery
-$(function () {
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const request = async (url, options = {}) => {
+  options.headers = options.headers || {}
+  options.headers['Content-Type'] = 'Content-Type: application/json; charset=utf-8'
+
+  return fetch(url, options).then(res => {
+    if (res.ok) {
+      return res.json()
+    } else {
+      return Promise.reject(res)
+    }
+  })
+}
+
+const main = () => {
+
   const articleTitle = $('#article-title')
   const articleContent = $('#article-content')
   const articleComment = $('#article-comment')
@@ -7,7 +23,9 @@ $(function () {
   const userSelect = $('#user-select')
   const userInfoDiv = $('#user-info')
   const upVoteArticle = $('#article-up-vote')
-  const userHeader = $('.user-header')
+  const downVoteArticle = $('#article-down-vote')
+  const userHeader = $$('.user-header')
+  const commentHeader = $('#comment-header')
   const commentContent = $('#comment-text')
   const submitComment = $('#submit-comment')
   const commentList = $('#comment-list')
@@ -20,73 +38,81 @@ $(function () {
 
   const handleArticleModel = (d) => {
     article = d
-    articleTitle.html(d.title)
-    articleContent.html(d.content.split('。').join('。<br>'))
-    upVoteArticle.html('点赞(' + (article.upVoteCount) + ')')
+    articleTitle.innerText = d.title
+    articleContent.innerHTML = d.content.split('。').join('。<br>')
+    upVoteArticle.innerText = '点赞(' + (article.upVoteCount) + ')'
+    downVoteArticle.innerText = '反对(' + (article.downVoteCount) + ')'
+    articleComment.innerText = '评论(' + (article.commentCount) + ')'
   }
 
   const handleUserModel = d => {
     users = d
     selectedUserId = d[0].id
     userInfo = d[0]
-    userHeader[0].html(userInfo.name[0])
+    commentHeader.innerText = userInfo.name[0]
     if (userInfo.upVoteArticles.some(record => record.article === article.id)) {
-      upVoteArticle.html('已点赞(' + article.upVoteCount + ')')
+      upVoteArticle.innerText = '已点赞(' + article.upVoteCount + ')'
     }
-    userInfoDiv.html(JSON.stringify(userInfo))
-    $.each(d, (i, user) => {
-      userSelect.append($('<option>', {
-        text: user.name,
-        value: user.id
-      }))
+    userInfoDiv.innerText = JSON.stringify(userInfo)
+    d.forEach(user => {
+      userSelect.insertAdjacentHTML('beforeend', `<option value="${user.id}">${user.name}</option>`)
     })
   }
 
   const handleCommentsModel = d => {
     comments = d
-    const ul = $('<ul/>')
-    commentList.append(ul)
-    d.map(comment => {
-      ul.append($(`<li>
-    <span class="user-header">${users.filter(user => user.id === comment.userId)[0].name[0]}</span>
-    <p>${comment.content}</p>
-</li>`))
-    })
+    commentList.innerHTML = `<ul>
+      ${d.map(comment => `<li>
+          <span class="user-header">${users.filter(user => user.id === comment.userId)[0].name[0]}</span>
+          <time>${new Date(comment.createTime).toLocaleString()}</time>
+          <p>${comment.content}</p>
+      </li>`)}
+    </ul>`
   }
 
   const handleError = error => {
-
+    if (error instanceof Response) {
+      errorMessageP.innerText = ` url: ${error.url} ${error.status}`
+    }
   }
 
   const refreshPage = () => {
-    Promise.all([$.get('/article/0001'), $.get('/user'), $.get('/comments/article/0001')]).then(([article, user, commentsOfArticle]) => {
+    Promise.all([request('/article/0001'), request('/user'), request('/comments/article/0001')]).then(([article, user, commentsOfArticle]) => {
 
       handleArticleModel(article)
       handleUserModel(user)
       handleCommentsModel(commentsOfArticle)
 
-    }).catch(e => errorMessageP.html(e.statusText).show())
+    }).catch(e => handleError(e))
   }
 
   refreshPage()
 
-  userSelect.on('change', function () {
+  /**
+   * 切换用户
+   */
+  userSelect.onchange = function () {
+
     selectedUserId = this.value
     userInfo = users.filter(user => user.id === selectedUserId)[0]
     userHeader.html(userInfo.name[0])
     userInfoDiv.html(JSON.stringify(userInfo))
-  })
-  upVoteArticle.on('click', function () {
+
+    if (userInfo.upVoteArticles.find(record => record.article === article.id) > -1) {
+      upVoteArticle.html('已点赞(' + (article.upVoteCount) + ')')
+      upVoteArticle.disabled = true
+    }
+  }
+  upVoteArticle.onclick = function () {
     if (userInfo.upVoteArticles.find(record => record.article === article.id)) {
       return
     }
-    upVoteArticle.html('已点赞(' + (article.upVoteCount + 1) + ')')
-    $.post('/upVoteArticle', {user: selectedUserId, article: article.id, timestamp: Date.now()})
+    $.post('/user/upVoteArticle', {userId: selectedUserId, articleId: article.id})
       .then((d => {
         console.log(d)
       }))
-  })
-  submitComment.on('click', function () {
+  }
+  submitComment.onclick = function () {
     const _commentContent = commentContent.html().trim()
     if (!_commentContent) return
     $.ajax({
@@ -94,12 +120,17 @@ $(function () {
       method: 'PUT',
       data: {content: _commentContent, userId: userInfo.id, articleId: article.id}
     })
-      .then(d => {})
+      .then(d => {
+        if (d.content === _commentContent) {
+          $.get('/comments/article/0001').then(d => {
+            handleCommentsModel(d)
+          })
+        }
+      })
       .catch(e => errorMessageP.html(e.statusText).show())
-  })
+  }
 
-  articleComment.on('click', function () {
+}
 
-  })
-
-})
+document.addEventListener('DOMContentLoaded', main)
+// document.addEventListener('readystatechange', main)
